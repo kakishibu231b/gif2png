186956f8ce02914d410d28269816bf057a29da19
柿渋 <kakishibu231b@gmail.com>
2021-11-01 16:48:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.vb
@@ -0,0 +1,44 @@
+Imports System
+Imports System.Drawing
+Imports System.Drawing.Imaging
+
+Module Program
+    Sub Main(args As String())
+
+        Dim imgInputImage As Image = Nothing
+
+        Try
+            If args.Length < 1 Then
+                GoTo NORMAL_END
+            End If
+
+            Dim strInputPath As String = args(0)
+            Dim strInputDir As String = System.IO.Path.GetDirectoryName(strInputPath)
+            Dim strInputFileNameWithoutExtension As String = System.IO.Path.GetFileNameWithoutExtension(strInputPath)
+            Dim strOputputDir As String = System.IO.Path.Combine(strInputDir, strInputFileNameWithoutExtension)
+            Dim objOutputDir As System.IO.DirectoryInfo = System.IO.Directory.CreateDirectory(strOputputDir)
+
+            imgInputImage = Image.FromFile(strInputPath)
+            Dim fdimInputImage As New FrameDimension(imgInputImage.FrameDimensionsList(0))
+            Dim intFrameCount As Integer = imgInputImage.GetFrameCount(fdimInputImage)
+
+            For ii As Integer = 1 To intFrameCount
+                imgInputImage.SelectActiveFrame(fdimInputImage, ii - 1)
+                Dim strOputputPath As String = System.IO.Path.Combine(strOputputDir, ii.ToString & ".png")
+                imgInputImage.Save(strOputputPath, System.Drawing.Imaging.ImageFormat.Png)
+            Next
+
+        Catch ex As Exception
+            WriteLine(ex.Message)
+
+        Finally
+            If Not IsNothing(imgInputImage) Then
+                imgInputImage.Dispose()
+            End If
+
+        End Try
+
+NORMAL_END:
+
+    End Sub
+End Module
---
+++ README.md
@@ -0,0 +1,2 @@
+# gif2png
+Split gif file with multi frame to single frame png files.
